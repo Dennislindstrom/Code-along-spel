4220ffb107ba7bb4ec8a61eb47552a1dd87a3705
Dennislindstrom <113332155+Dennislindstrom@users.noreply.github.com>
2022-09-12 15:05:01 +0200

    Welcome user
    Welcome user and decide health

--- Code along, spel/Program.cs
+++ Code along, spel/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Welcome player to the game
+// Decide monster and player health
+// See who goes first
+
+
+//loop
+//Attack
+//Calculate damage
+//Subtract damage
+//Change turn 
+
+
+
+
+//VARIABLES
+int playerHealth;
+int monsterHealth;
+
+
+
+//METHOD CALLS
+WelcomePlayer();
+DecidePlayerAndMonsterHealth();
+
+
+//METHODS
+
+//Welcome player to the game
+void WelcomePlayer()
+{
+    Console.WriteLine("--------------------------");
+    Console.WriteLine("Welcome to the battle game");
+    Console.WriteLine("    Winner of all awards  ");
+    Console.WriteLine("--------------------------");
+}
+
+
+// Ask player for monster and player health, funktion.
+void DecidePlayerAndMonsterHealth()
+{
+    Console.WriteLine();
+    Console.Write("What should player HP be? ");
+    int playerhealth = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.Write("Great! What should monster HP be? ");
+    int monsterhealth = Convert.ToInt32(Console.ReadLine());
+
+}
